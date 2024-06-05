e1ac2118f98849dc8e73bfa1ab024e5e1351e132
vishnureddy6301 <Vishnureddy6301@gmail.com>
2024-06-05 11:24:34 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react'
-// import Api from './Components/Api'
-//import './App.css'
+
 
 // import Curd from './Components/Curd'
 
-//  import Contact from './Components/Contact'
-import Dash from './Components/Dash'
-//import Main from './Components/Main'
+ import Contact from './Components/Contact'
+// import Dash from './Components/Dash'
+
 
 const App = () => {
   return (
     <div>
-    {/* <Contact/> */}
+    <Contact/>
       
      {/* <Curd/>  */}
-     {/* <Main/> */}
-     <Dash/>
-     {/* <Api/> */}
+  
+     {/* <Dash/> */}
+   
      
     </div>
   )
